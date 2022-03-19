66d7355e0de6f59222d9ee8980aba55a7d655e21
danielnels <danieln@newground.net.au>
2022-03-19 19:29:52 +0800

    create handlebars

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-        expires: 60000 * 2
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
+  secret: 'Super secret secret',
+  cookie: {
+    expires: 60000 * 2
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
@@ -36,5 +36,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log("Now listening http://localhost:" + PORT));
+  app.listen(PORT, () => console.log('Now listening http://localhost:' + PORT));
 });
