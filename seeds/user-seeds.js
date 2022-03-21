8d27b050bc9470ebdd7929dc70bc4f736a57e0c2
danielnels <danieln@newground.net.au>
2022-03-22 07:42:50 +0800

    Completed formatting

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,26 +1,27 @@
+// eslint-disable-next-line no-unused-vars
 const sequelize = require('../config/connection');
 const { User } = require('../models');
 
 const userdata = [{
-        username: 'dannels',
-        email: 'dn@gmail.com',
-        password: '123456'
-    },
-    {
-        username: 'marleyn',
-        email: 'mn@gmail.com',
-        password: '123456'
-    },
-    {
-        username: 'kristenn',
-        email: 'kn@gmail.com',
-        password: '123456'
-    },
-    {
-        username: 'jackn',
-        email: 'jack@gmail.com',
-        password: '123456'
-    }
+  username: 'dannels',
+  email: 'dn@gmail.com',
+  password: '123456'
+},
+{
+  username: 'marleyn',
+  email: 'mn@gmail.com',
+  password: '123456'
+},
+{
+  username: 'kristenn',
+  email: 'kn@gmail.com',
+  password: '123456'
+},
+{
+  username: 'jackn',
+  email: 'jack@gmail.com',
+  password: '123456'
+}
 ];
 
 const seedUsers = () => User.bulkCreate(userdata, { individualHooks: true });
